4fe523e3770d1c95c986aa48f1f64ac993d95652
Martin Fenner <martin@front-matter.io>
2024-04-08 08:10:50 +0200

    fix path to public dir

--- main.go
+++ main.go
@@ -63,7 +63,7 @@ func main() {
 
 	// serves static files from the provided public dir (if exists)
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("/pb/pb_public"), false))
 		return nil
 	})
 
